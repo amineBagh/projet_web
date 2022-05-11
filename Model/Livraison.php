12cbeda917a6607449632679cabe0b0b28ea0555
rayenebr <rayen.benromdhane@esprit.tn>
2022-05-11 16:37:34 +0100

    ww

---
+++ Model/Livraison.php
@@ -0,0 +1,72 @@
+<?php
+class Livraison {
+private $id ;
+private $nom;
+private $prenom;
+private $adresse;
+private $tel;
+private $mail;
+private $livreurId;
+
+function __construct( $nom,$prenom,$adresse,$tel,$mail,$livreurId){
+    $this->nom=$nom;
+    $this->prenom=$prenom;
+    $this->adresse=$adresse;
+    $this->tel=$tel;
+    $this->mail=$mail;
+    $this->livreurId=$livreurId;
+    //$this->file_path=$file_path	;  
+}
+
+function getId(){
+    return $this->id;}
+
+
+    function getNom(){
+        return $this->nom;}
+
+        function getPrenom(){
+            return $this->prenom;}
+
+            function getAdresse(){
+                return $this->adresse;}
+                function getTel(){
+                    return $this->tel;}
+                    function getMail(){
+                        return $this->mail;}
+
+   function getLd(){
+        return $this->livreurId;}
+
+
+
+        function setNom(string $nom){
+            $this->nom=$nom;}
+
+            function setPreom(string $prenom){
+                $this->prenom=$prenom;}
+
+                function setAdresseom(string $adresse){
+                    $this->adresse=$adresse;}
+
+                    function setTel(string $tel){
+                        $this->tel=$tel;}
+
+                        function setMail(string $mail){
+                            $this->mail=$mail;}
+
+                        function setLd(int $livreurId){
+                            $this->livreurId=$livreurId;}
+
+}
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ Model/Livreur.php
@@ -0,0 +1,51 @@
+<?php
+class Livreur {
+private $id ;
+private $nom;
+private $prenom;
+private $cin;
+private $num_tel;
+
+function __construct( $nom,$prenom,$cin,$num_tel	){
+    $this->nom=$nom;
+    $this->prenom=$prenom;
+    $this->cin=$cin;
+    $this->num_tel=$num_tel;
+    
+    //$this->file_path=$file_path	;
+  
+}
+
+function getId(){
+    return $this->id;}
+    
+    function getNom(){
+        return $this->nom;}
+
+        function getPrenom(){
+            return $this->prenom;}
+
+            function getCin(){
+                return $this->cin;}
+
+            function getNum(){
+                return $this->num_tel;}
+
+                function setNom(string $nom){
+                    $this->nom=$nom;}
+                    function setPrenom(string $prenom){
+                        $this->prenom=$prenom;}
+                        function setCin(string $cin){
+                            $this->cin=$cin;}
+
+                        function setNum(string $num_tel){
+                            $this->num_tel=$num_tel;}
+                
+
+}
+
+
+
+
+
+?>
